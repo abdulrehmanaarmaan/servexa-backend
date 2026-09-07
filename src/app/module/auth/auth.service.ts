23ff296c9e6847bc16f518d1e127c2f452572d8b
AbdulRehmanAarmaan <abdulrehmanaarmaan@gmail.com>
2026-09-07 12:38:12 -0300

    feat: add service request cancellation workflow

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -1,6 +1,5 @@
 /** biome-ignore-all lint/style/useConst: <explanation> */
 import bcrypt from "bcryptjs";
-import crypto from "crypto";
 import type { JwtPayload, SignOptions } from "jsonwebtoken";
 import httpStatus from "http-status";
 import { ILoginUserPayload, IRegisterPatientPayload, IRequestUser } from "./auth.interface.js";
